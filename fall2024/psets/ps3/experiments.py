6fa392e542b6b935b4763bdebc816b2975d959af
Dhamar Carrillo <dhamarcarrillo@college.harvard.edu>
2024-10-02 23:57:25 -0400

    finished

--- fall2024/psets/ps3/experiments.py
+++ fall2024/psets/ps3/experiments.py
@@ -27,21 +27,25 @@ thirteen_id = 7
 temp_id = 8   # Used only in prog2
 W_id = 9       # Used only in prog2
 
-# TODO: Fill in prog1 with the first RAM program provided in the homework.
+# todo: Fill in prog1 with the first RAM program provided in the homework.
 prog1 = [8, 
     ['assign', zero_id, 0],
     ['assign', one_id, 1], 
     ['assign', thirteen_id, 13],
     ['assign', output_len_id, 1], 
     ['assign', output_ptr_id, 0],
-    # TODO: lines 5-8 from pseudocode
+    # todo: lines 5-8 from pseudocode
+    ['assign', result_id, 13],
+    ['read', counter_id, zero_id],
+    ['goto', counter_id, 11],
+    ['*', result_id, result_id, result_id],
     ['-', counter_id, counter_id, one_id],
     ['goto', zero_id, 7],
     ['*', result_id, result_id, thirteen_id],
     ['write', output_ptr_id, result_id]
 ]
 
-# TODO: Fill in prog2 with the second RAM program provided in the homework.
+# todo: Fill in prog2 with the second RAM program provided in the homework.
 prog2 = [10, 
     ['assign', zero_id, 0],
     ['assign', one_id, 1], 
@@ -57,7 +61,13 @@ prog2 = [10,
     ['*', temp_id, temp_id, W_id],
     ['-', result_id, result_id, temp_id],
     ['-', counter_id, counter_id, one_id],
-    # TODO: lines 14-19 from pseudocode
+    # todo: lines 14-19 from pseudocode
+    ['goto', zero_id, 8],
+    ['*', result_id, result_id, thirteen_id],
+    ['/', temp_id, result_id, W_id],
+    ['*', temp_id, temp_id, W_id],
+    ['-', result_id, result_id, temp_id],
+    ['write', output_ptr_id, result_id]
 ]
 
 
--- fall2024/psets/ps3/simulator.py
+++ fall2024/psets/ps3/simulator.py
@@ -44,34 +44,39 @@ def executeProgram(programArr, inputArr):
             memory[variableList[ops[0]]] = variableList[ops[1]]
         if cmd == "assign":
             # ['assign', i, j]: assign var_i to the value j
-            # TODO: Implement assign.
-            pass
+            # todo: Implement assign.
+            variableList[ops[0]] = ops[1]
             
         # Arithmetic commands
         if cmd == "+":
             # ['+', i, j, k]: compute (var_j + var_k) and store in var_i
-            # TODO: Implement addition.
-            pass
+            # todo: Implement addition.
+            variableList[ops[0]] = variableList[ops[1]] + variableList[ops[2]]
         if cmd == "-":
             # ['-', i, j, k]: compute max((var_j - var_k), 0) and store in var_i.
-            # TODO: Implement subtraction.
-            pass
+            # todo: Implement subtraction.
+            variableList[ops[0]] = max((variableList[ops[1]] - variableList[ops[2]]), 0)
         if cmd == "*":
             # ['*', i, j, k]: compute (var_j * var_k) and store in var_i.
-            # TODO: Implement multiplication.
-            pass
+            # todo: Implement multiplication.
+            variableList[ops[0]] = variableList[ops[1]] * variableList[ops[2]]
         if cmd == "/":
             #  ['/', i, j, k]: compute (var_j // var_k) and store in var_i.
             # Note that this is integer division. You should return an integer, not a float.
             # Remember division by 0 results in 0.
-            # TODO: Implement division.
-            pass
+            # todo: Implement division.
+            if variableList[ops[2]] == 0:
+                variableList[ops[0]] = 0
+            else:
+                variableList[ops[0]] = variableList[ops[1]] //variableList[ops[2]]
             
         # Control commands
         if cmd == "goto":
             # ['goto', i, j]: if var_i is equal to 0, go to line j
-            # TODO: Implement goto.
-            pass
+            # todo: Implement goto.
+            if variableList[ops[0]] == 0:
+                programCounter = ops[1]
+                continue
         
         programCounter += 1
     
